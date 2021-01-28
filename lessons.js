88aa824daaffbcd5d1aed1ae705b63e95862ccef
Farhan Vakil <aneefari@yahoo.com>
2021-01-28 13:00:31 +0000

    Initial commit

---
+++ lessons.js
@@ -0,0 +1,92 @@
+let lessons= [
+    {
+        id:1,
+        name: 'Football',
+        image:'football.jpg',
+        location:'Chelsea',
+        price:150,
+        availability: 7
+    
+    },
+
+    {
+        id:2,
+        name: 'Cricket',
+        image:'cricket.jpg',
+        location:'Harrow',
+        price:200,
+        availability:5
+    },
+
+    {
+        id:3,
+        name: 'Swimming',
+        image:'swimming.jpg',
+        location:'Acton Town',
+        price:150,
+        availability:5
+    },
+    
+    {
+        id:4,
+        name: 'Rugby',
+        image:'rugby.jpg',
+        location:'Watford',
+        price:150,
+        availability:5
+    },
+
+    {
+        id:5,
+        name: 'Snooker',
+        image:'snooker.jpg',
+        location:'Wembley Park',
+        price:50,
+        availability:5
+    },
+
+    {
+        id:6,
+        name: 'Bowling',
+        image:'bowling.jpg',
+        location:'Park Royal',
+        price:50,
+        availability:5
+    },
+
+    {
+        id:7,
+        name: 'Tennis',
+        image:'tennis.jpg',
+        location:'Wimbledon',
+        price:200,
+        availability:5
+    },
+
+    {
+        id:8,
+        name: 'Golf',
+        image:'golf.jpg',
+        location:'Watford',
+        price:100,
+        availability:5
+    },
+
+    {
+        id:9,
+        name: 'Gym',
+        image:'gym.jpg',
+        location:'Park Royal',
+        price:50,
+        availability:5
+    },
+
+    {
+        id:10,
+        name: 'Basketball',
+        image:'basketball.jpg',
+        location:'Park Royal',
+        price:100,
+        availability:5
+    },
+];
